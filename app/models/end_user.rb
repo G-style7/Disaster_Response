faa4d6415acb2fc70e7302d1f0d3764ff8273862
G-style7 <miyasato1989@gmail.com>
2022-11-04 06:03:25 +0000

    footer_now

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -7,7 +7,7 @@ class EndUser < ApplicationRecord
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :helps, dependent: :destroy
-  
+
   #重複していないemailであるか（一意性、ユニーク (uniqueness)とは、誰かまたは何かが他のものと比較して異なる状態または状態のこと。）
   validates :email, uniqueness: true
   validates :last_name,:first_name,:last_name_kana,:first_name_kana,:birthday,:gender,:nickname,:postal_code,:address,:telephone_number, presence: true
@@ -22,5 +22,13 @@ class EndUser < ApplicationRecord
 
   def full_name_kana
     last_name_kana+first_name_kana
+  end
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
