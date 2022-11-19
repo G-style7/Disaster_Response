215e7ab291b9bcb02e7debb054ed3799dea69d54
G-style7 <miyasato1989@gmail.com>
2022-11-19 07:32:57 +0000

    GoogleAPI_fin

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,14 @@ module DisasterResponse
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # デフォルトのロケールを:en以外に変更する
     config.i18n.default_locale = :ja
-    
+
+    # I18nライブラリに訳文の探索場所を指示する
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
+
+
+
     config.time_zone = 'Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
