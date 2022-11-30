4ae58fb089b85ff02395806c3dd923bb7fedf19b
G-style7 <miyasato1989@gmail.com>
2022-11-30 13:43:58 +0000

    contact_now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,14 +97,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp 
   config.action_mailer.smtp_settings = {
-      port: 587,
-      address: 'smtp.gmail.com',
+      port: 587, #ポートがサーバーの出入り口
+      address: 'smtp.gmail.com', #Gmailのメール送信サーバーを示している
       domain: 'smtp.gmail.com',
-      user_name: ENV['SMTP_USERNAME'],
+      user_name: ENV['SMTP_USERNAME'], #Gmailログインするための情報
       password: ENV['SMTP_PASSWORD'],
-      enable_starttls_auto: true
+      enable_starttls_auto: true #暗号化
   }
 
   # Inserts middleware to perform automatic connection switching.
